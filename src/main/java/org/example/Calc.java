d08506ca5390f22df03a2c5d82ec32202f4667c6
doun9711-ai <doun9711@gmail.com>
2025-12-01 15:46:12 +0900

    10 + 20 + 30

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -11,19 +11,26 @@ public class Calc {
 
         if (needToPlus) {
             bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
+        }
+        else if (needToMinus) {
             bits = exp.split(" - ");
         }
 
+
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
-        int c = Integer.parseInt(bits[2]);
-
+        int c = 0;
+        if(bits.length >2) {
+            c = Integer.parseInt(bits[2]);
+        }
         if (needToPlus) {
             return a + b + c;
-        } else if (needToMinus) {
+        }
+        else if (needToMinus) {
             return a - b;
         }
+
+
 
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
     }
