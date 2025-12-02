4a2e17cec1bb37e3709e13f3954065ced6e765e2
doun9711-ai <doun9711@gmail.com>
2025-12-02 14:02:17 +0900

    ((20 + 20)) + 20

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -90,20 +90,29 @@ public class CalcTest {
     public void test14() {
         assertThat(Calc.run("10 * 10 * 10 * 100")).isEqualTo(100000);
     }
+
     @Test
     @DisplayName("10 + 5 * 2 == 20")
     public void test15() {
         assertThat(Calc.run("10 + 5 * 2")).isEqualTo(20);
     }
+
     @Test
     @DisplayName("20 + 10 + 5 * 2 == 40")
     public void test16() {
         assertThat(Calc.run("20 + 10 + 5 * 2")).isEqualTo(40);
     }
+
     @Test
     @DisplayName("10 * 20 + 10 + 5 * 2 == 220")
     public void test17() {
         assertThat(Calc.run("10 * 20 + 10 + 5 * 2")).isEqualTo(220);
     }
 
+    @Test
+    @DisplayName("((20 + 20)) + 20 == 60")
+    public void test18() {
+        assertThat(Calc.run("((20 + 20)) + 20")).isEqualTo(60);
+
+    }
 }
